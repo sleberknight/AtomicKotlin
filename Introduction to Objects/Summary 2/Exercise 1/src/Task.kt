44fa05313f32c40be2ac3bbbb184bad2470bc47b
Scott Leberknight <scottleber@gmail.com>
2019-01-25 20:09:26 -0500

    Finished summary 2

--- Introduction to Objects/Summary 2/Exercise 1/src/Task.kt
+++ Introduction to Objects/Summary 2/Exercise 1/src/Task.kt
@@ -2,10 +2,64 @@ package summary21
 
 import atomictest.eq
 
-val intToRomanNumeralMap: Map<Int, String> = TODO()
+val intToRomanNumeralMap: Map<Int, String> = mapOf(
+        1 to "I",
+        2 to "II",
+        3 to "III",
+        4 to "IV",
+        5 to "V",
+        6 to "VI",
+        7 to "VII",
+        8 to "VIII",
+        9 to "IX",
+        10 to "X",
+        20 to "XX",
+        30 to "XXX",
+        40 to "XL",
+        50 to "L",
+        60 to "LX",
+        70 to "LXX",
+        80 to "LXXX",
+        90 to "XC",
+        100 to "C",
+        200 to "CC",
+        300 to "CCC",
+        400 to "CD",
+        500 to "D",
+        600 to "DC",
+        700 to "DCC",
+        800 to "DCCC",
+        900 to "CM",
+        1000 to "M",
+        2000 to "MM",
+        3000 to "MMM"
+)
 
 fun convertToRoman(number: Int): String {
-    TODO()
+    require(number > 0) { "number must be more than zero"}
+    require(number < 4000) { "number must be 3,999 or less" }
+
+    val romanFromMap = intToRomanNumeralMap[number]
+    if (romanFromMap != null) {
+        return romanFromMap
+    }
+
+    var factor = 1000
+    var remaining = number
+    val roman = StringBuilder()
+
+    while (factor > 0) {
+        val factorNumber = (remaining / factor) * factor
+
+        if (factorNumber > 0) {
+            roman.append(intToRomanNumeralMap.getValue(factorNumber))
+        }
+
+        remaining %= factor
+        factor /= 10
+    }
+
+    return roman.toString()
 }
 
 fun main(args: Array<String>) {
--- Introduction to Objects/Summary 2/Exercise 2/src/Task.kt
+++ Introduction to Objects/Summary 2/Exercise 2/src/Task.kt
@@ -2,10 +2,33 @@ package summary22
 
 import atomictest.eq
 
-val romanNumeralToIntMap: Map<String, Int> = TODO()
+val romanNumeralToIntMap: Map<Char, Int> = mapOf(
+        'M' to 1000,
+        'D' to 500,
+        'C' to 100,
+        'L' to 50,
+        'X' to 10,
+        'V' to 5,
+        'I' to 1
+)
 
 fun convertFromRoman(roman: String): Int {
-    TODO()
+    var result = 0
+    var max = 0
+
+    for (romanNumeral in roman.reversed()) {
+        val value = romanNumeralToIntMap.getValue(romanNumeral)
+
+        if (value >= max) {
+            result += value
+            max = value
+        }
+        else {
+            result -= value
+        }
+    }
+
+    return result
 }
 
 fun main(args: Array<String>) {
